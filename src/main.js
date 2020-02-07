3ff29aa5a72d546e6b38d2a5f4a7ea902179d125
osushicrusher <osushicrusher@gmail.com>
2020-02-07 09:50:09 +0900

    hello

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faSearch)
+
+Vue.use(VueRouter)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
