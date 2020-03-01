79b6c3385f89aeb8c195b16d4f84ac57c6d5ed65
osushicrusher <osushicrusher@gmail.com>
2020-03-01 23:44:56 +0900

    master

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faSearch)
+library.add(fas)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
